35f7ad65ff3d5013e1420c2a506be666100baaea
Dima Popov <popov.dmitriiy@gmail.com>
2026-01-27 19:55:38 +0100

    Initial commit

---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+	title: "Dima's Portfolio Site",
+	description:
+		"Welcome to my portfolio site! I'm a self-taught artist and a software engineer. I see the beauty in inperfection",
+};
