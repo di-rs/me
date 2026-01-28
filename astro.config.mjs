76dcfd7121eaa434e561f08783bf6e87ed2f6e69
Dima Popov <popov.dmitriiy@gmail.com>
2026-01-28 23:19:53 +0100

    feat: add site property

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import vue from "@astrojs/vue";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  site: "https://profile.dimaportish.com",
+  integrations: [sitemap(), vue()],
 
   vite: {
     plugins: [tailwindcss()],
