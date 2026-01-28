e5995a5d5f775f1be5ee40beb0a5ef9cb15144fd
Dima Popov <popov.dmitriiy@gmail.com>
2026-01-28 20:16:57 +0100

    style fixs

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,4 +1,4 @@
-export const siteData = {
+export const baseData = {
   title: "Dima Portfolio Site",
   description:
     "Welcome to my portfolio site! I'm a Sunday artist and a software engineer. I see the beauty in imperfection",
